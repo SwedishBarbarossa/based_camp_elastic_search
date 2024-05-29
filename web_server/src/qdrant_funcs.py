430029cbfc1bd218287111c5d11aaa2f86108d34
SwedishBarbarossa <erik4x31550n@protonmail.com>
2024-05-29 02:08:36 +0200

    add channel index and quantization

--- web_server/src/qdrant_funcs.py
+++ web_server/src/qdrant_funcs.py
@@ -18,6 +18,7 @@ def query_qdrant(
     client: QdrantClient,
     q_type: Literal["sym", "asym"] = "sym",
 ) -> list[list]:
+    """Query Qdrant within channels and return the results"""
     _filter = models.Filter(
         should=[
             models.FieldCondition(
@@ -52,18 +53,56 @@ def query_qdrant(
 
 
 def create_qdrant_collection(collection: str, client: QdrantClient, dim: int) -> None:
+    """Creates a collection if it doesn't exist yet"""
+    collection_config = {
+        "collection_name": collection,
+        "optimizers_config": models.OptimizersConfigDiff(memmap_threshold=20000),
+        "hnsw_config": models.HnswConfigDiff(on_disk=True, m=32, ef_construct=256),
+        "quantization_config": models.ScalarQuantization(
+            scalar=models.ScalarQuantizationConfig(
+                type=models.ScalarType.INT8,
+                quantile=0.999,
+                always_ram=True,
+            ),
+        ),
+    }
     if client.collection_exists(collection):
+        client.update_collection(**collection_config)
+    else:
+        client.create_collection(
+            vectors_config=models.VectorParams(
+                size=dim, distance=models.Distance.COSINE
+            ),
+            **collection_config,
+        )
+
+
+def payload_index_exists(
+    client: QdrantClient, collection_name: str, payload_field: str
+):
+    """Check if payload index exists"""
+    collection_info = client.get_collection(collection_name)
+    if payload_field in collection_info.payload_schema:
+        return True
+    return False
+
+
+def create_qdrant_index(
+    payload_key: str, collection: str, client: QdrantClient
+) -> None:
+    """Creates a payload index for the collection if it doesn't exist yet"""
+    if payload_index_exists(client, collection, payload_key):
         return
 
-    client.create_collection(
+    client.create_payload_index(
         collection_name=collection,
-        vectors_config=models.VectorParams(size=dim, distance=models.Distance.COSINE),
-        optimizers_config=models.OptimizersConfigDiff(memmap_threshold=20000),
-        hnsw_config=models.HnswConfigDiff(on_disk=True, m=32, ef_construct=256),
+        field_name=payload_key,
+        field_schema=models.PayloadSchemaType.TEXT,
     )
 
 
 def _name_to_payload(name: str) -> tuple[str, str, str, int, int]:
+    """Parse filename into (q_type, channel, id, start, end)"""
     split = name.removesuffix(".npy").split(" ")
     try:
         return split[0], split[1], split[2], int(split[3]), int(split[4])
@@ -77,9 +116,11 @@ def add_to_qdrant(
     client: QdrantClient,
     dims: ModelDims,
 ) -> None:
-
+    """Add embeddings in embeddings_dir to Qdrant"""
     create_qdrant_collection("asym", client, dims["asymmetric"])
+    create_qdrant_index("channel", "asym", client)
     create_qdrant_collection("sym", client, dims["symmetric"])
+    create_qdrant_index("channel", "sym", client)
 
     already_added: set[str] = set()
     if os.path.exists(record_path):
